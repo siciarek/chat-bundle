c698eae00afee51d41e158014a8c7b58ae303f09
Jacek Siciarek <jacek.siciarek@escore.pl>
2016-01-27 11:51:36 +0100

    Add Base Enities

---
+++ src/Siciarek/ChatBundle/Entity/ChatMessage.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Siciarek\ChatBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Siciarek\ChatBundle\Entity\ChatMessage
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="chat_message")
+ * @ORM\Entity(repositoryClass="ChatMessageRepository")
+ */
+class ChatMessage {
+
+    use ORMBehaviors\Blameable\Blameable;
+    use ORMBehaviors\Timestampable\Timestampable;
+    use ORMBehaviors\SoftDeletable\SoftDeletable;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ChatRoom", inversedBy="messages")
+     */
+    private $room;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+}
+
