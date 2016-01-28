ef10e75a858f3cb8ea0b5a900956bbad4da5684b
Jacek Siciarek <siciarek@gmail.com>
2016-01-28 22:49:27 +0100

    Add exceptions

---
+++ src/Siciarek/ChatBundle/Model/ChatChannelException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Siciarek\ChatBundle\Model;
+
+class ChatChannelException extends \Exception
+{
+
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, 4561237, $previous);
+    }
+
+}
